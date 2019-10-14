eef540d20adff464e1d523bc44590ff69d820d23
MZMZz <51102140+MZMZz@users.noreply.github.com>
2019-10-14 18:09:48 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NDUyOTEyNDQxNjY0MzM5OTc5.XaSEMA.laqpN24mdQZKScOrlQ7XXv7VUus', 
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: '1',
+    devs: ['449729950849826816']
+}
